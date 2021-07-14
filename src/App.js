2a479a9645653382f669eb3113e68d8724499027
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-14 13:37:47 +0300

    Создан каркас для компонента MainPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import {HashRouter, Switch, Route} from 'react-router-dom';
+import MainPage from './Main/MainPage/MainPage';
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            Need for drive
-        </div>
+        <HashRouter>
+            <Switch>
+                <Route exact path="/" component={MainPage}/>
+            </Switch>
+        </HashRouter>
     );
 }
 
