502ee7c041fd3f60e75a04bd032b565051c3ce2a
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-15 13:29:14 +0300

    v-01_f-01 Добавлено переключение стилей для кнопки меню

--- src/Main/Menu/Menu.js
+++ src/Main/Menu/Menu.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ReactComponent as TwoLines} from '../../content/two_lines.svg';
 import {ReactComponent as OneLine} from '../../content/one_line.svg';
 import style from './Menu.module.scss';
 
 function Menu() {
+    let [opened, setOpened] = useState(false);
+
+    let menuButtonClickHandler = () => {
+        setOpened(oldValue => !oldValue)
+    }
+
     return (
-        <div className={style.menu}>
-            <div className={style.menu__button}>
+        <div className={style.menu + (opened ? ` ${style.opened}` : '')}>
+            <div className={style.menu__button} onClick={menuButtonClickHandler}>
                 <TwoLines/>
                 <OneLine/>
                 <OneLine/>
