8b0f8739fa2e43d9ff7a33f186999f1dc6bc92e0
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-15 13:18:49 +0300

    v-01_f-01 Создана первая версия стилей для кнопки меню

--- src/Main/Menu/Menu.js
+++ src/Main/Menu/Menu.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {ReactComponent as TwoLines} from '../../content/two_lines.svg';
+import {ReactComponent as OneLine} from '../../content/one_line.svg';
 import style from './Menu.module.scss';
 
 function Menu() {
     return (
         <div className={style.menu}>
-
+            <div className={style.menu__button}>
+                <TwoLines/>
+                <OneLine/>
+                <OneLine/>
+            </div>
         </div>
     );
 }
