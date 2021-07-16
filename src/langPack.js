2b10c775af08905d10468e9a4be67f7be526204f
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-16 18:00:26 +0300

    v-01_f-01 В langPack.js добавлены данные для слайдера

--- src/langPack.js
+++ src/langPack.js
@@ -18,5 +18,49 @@ export const LANG_PACK = {
             slogan: 'Per-minute car rental in your city',
             reservationButton: 'Reservation'
         }
+    },
+    'Slider': {
+        RUS: {
+            slidersData: [
+                {
+                    title: 'Бесплатная парковка',
+                    description: 'Оставляйте машину на платных городских парковках, в аэропортах и разрешенных местах, не нарушая ПДД.'
+                },
+                {
+                    title: 'Страховка',
+                    description: 'Полная страховка автомобиля'
+                },
+                {
+                    title: 'Бензин',
+                    description: 'Полный бак на любой заправке города за наш счет'
+                },
+                {
+                    title: 'Обслуживание',
+                    description: 'Наши автомобили проходят еженедельное ТО'
+                }
+            ],
+            moreButton: 'Подробнее'
+        },
+        ENG: {
+            slidersData: [
+                {
+                    title: 'Free parking',
+                    description: 'Leave your car in paid city parking lots, airports and permitted places without breaking traffic rules'
+                },
+                {
+                    title: 'Insurance',
+                    description: 'Full car insurance'
+                },
+                {
+                    title: 'Gasoline',
+                    description: 'A full tank at any gas station in the city at our expense'
+                },
+                {
+                    title: 'Service',
+                    description: 'Our vehicles are serviced weekly'
+                }
+            ],
+            moreButton: 'More details'
+        }
     }
 }
