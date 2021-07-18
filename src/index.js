56565057cacae45c3378999ac5dd668ff35871bf
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-18 12:10:46 +0300

    v-01_f-01 Добавлен es-6 promise

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss';
 
+import {polyfill} from 'es6-promise';
+
 import store from './store/store';
 import {Provider} from 'react-redux';
+
+polyfill();
 
 ReactDOM.render(
     <Provider store={store}>
