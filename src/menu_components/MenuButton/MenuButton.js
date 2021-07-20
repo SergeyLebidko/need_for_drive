ab4ddd1d33fd69f940bcfd98e7e6c3089393cd4f
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-20 15:21:46 +0300

    v-01_f-01 Создана первая версия отдельного компонента для кнопки меню

---
+++ src/menu_components/MenuButton/MenuButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {ReactComponent as TwoLines} from '../../content/images/icons/two_lines.svg';
+import {ReactComponent as OneLine} from '../../content/images/icons/one_line.svg';
+import './MenuButton.scss';
+
+function MenuButton({handleClick}) {
+    return (
+        <div className="menu_button" onClick={handleClick}>
+            <TwoLines/>
+            <OneLine/>
+            <OneLine/>
+        </div>
+    )
+}
+
+MenuButton.propTypes = {
+    handleClick: PropTypes.func
+}
+
+export default MenuButton;
