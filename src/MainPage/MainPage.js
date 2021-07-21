698c1b4b66c1dc0c870bfdcd3c822be87a18adb4
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-21 14:24:12 +0300

    v-01_f-01 Рефакторинг стилей для компонента MainPage

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Menu from '../menu_components/Menu/Menu';
 import Title from '../Title/Title';
 import Slider from '../slider_components/Slider/Slider';
-import style from './MainPage.module.scss';
+import './MainPage.scss';
 
 function MainPage() {
     return (
-        <div className={style.main_page}>
+        <div className="main_page">
             <Menu/>
             <Title/>
             <Slider/>
