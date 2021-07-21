93cc00f890f5befd60840f4f9dea190d7cee6bb6
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-21 16:05:39 +0300

    v-01_f-01 Отмена вынесения кода формирования данных слайдера в отдельную функцию

--- src/slider_components/Slider/Slider.js
+++ src/slider_components/Slider/Slider.js
@@ -3,26 +3,35 @@ import Slide, {PREV_SLIDE, CURRENT_SLIDE, NEXT_SLIDE} from '../Slide/Slide';
 import DotsBlock from '../DotsBlock/DotsBlock';
 import Arrow, {TO_LEFT_ARROW, TO_RIGHT_ARROW} from '../Arrow/Arrow';
 import PropTypes from 'prop-types';
-import {createSliderData} from '../../utils';
 import stateMapsFactory from '../../store/stateMaps';
 import dispatchMapsFactory from '../../store/dispatchMaps';
 import {connect} from 'react-redux';
+import {LANG_PACK} from '../../langPack';
+import slide0 from '../../content/images/slides/slide-0.png';
+import slide1 from '../../content/images/slides/slide-1.png';
+import slide2 from '../../content/images/slides/slide-2.png';
+import slide3 from '../../content/images/slides/slide-3.png';
 import './Slider.scss';
+
+const SLIDE_COUNT = 4;
 
 function Slider({lang}) {
     let [index, setIndex] = useState(0);
-    let [slidersData, setSlidersData] = useState([]);
-    let [slideButtonText, setSlideButtonText] = useState('');
 
-    let getNextIndex = index => index === (slidersData.length - 1) ? 0 : index + 1;
-    let getPrevIndex = index => index ? index - 1 : (slidersData.length - 1);
+    // Готовим данные слайдов
+    const slidersImages = [slide0, slide1, slide2, slide3];
+    const slidersButtonColors = ['dark_green', 'teal', 'brown', 'purple'];
+    const {slidersText, slideButtonText} = LANG_PACK['Slider'][lang];
+    const slidersData = [];
 
-    // В зависимости от выбранного языка - обновляем данные слайдов
-    useEffect(() => {
-        let {data, buttonText} = createSliderData(lang);
-        setSlidersData(data);
-        setSlideButtonText(buttonText);
-    }, [lang]);
+    for (let index = 0; index < SLIDE_COUNT; index++) slidersData.push({
+        ...slidersText[index],
+        image: slidersImages[index],
+        buttonColor: slidersButtonColors[index]
+    });
+
+    let getNextIndex = index => index === (SLIDE_COUNT - 1) ? 0 : index + 1;
+    let getPrevIndex = index => index ? index - 1 : (SLIDE_COUNT - 1);
 
     // Добавляем автоматическую перемотку слайдов по таймеру (каждые 10 сек)
     useEffect(() => {
@@ -32,6 +41,9 @@ function Slider({lang}) {
         return () => clearInterval(interval);
     }, []);
 
+    let prevIndex = getPrevIndex(index);
+    let nextIndex = getNextIndex(index);
+
     let handleRightArrowClick = () => setIndex(oldIndex => getNextIndex(oldIndex));
     let handleLeftArrowClick = () => setIndex(oldIndex => getPrevIndex(oldIndex));
 
@@ -40,7 +52,7 @@ function Slider({lang}) {
         if (dotIndex === index) return;
 
         let distance = Math.abs(dotIndex - index);
-        if (distance === 1 || distance === (slidersData.length - 1)) setIndex(dotIndex);
+        if (distance === 1 || distance === (SLIDE_COUNT - 1)) setIndex(dotIndex);
 
         // Если слайды не находятся рядом, то перематываем их в несколько шагов
         let interval = setInterval(() => {
@@ -53,8 +65,6 @@ function Slider({lang}) {
         }, 300);
     };
 
-    let prevIndex = getPrevIndex(index);
-    let nextIndex = getNextIndex(index);
     let currentSlidesData = [
         {
             ...slidersData[prevIndex],
@@ -77,7 +87,7 @@ function Slider({lang}) {
             )}
             <Arrow direction={TO_LEFT_ARROW} handleClick={handleLeftArrowClick}/>
             <Arrow direction={TO_RIGHT_ARROW} handleClick={handleRightArrowClick}/>
-            <DotsBlock dotsCount={slidersData.length} currentIndex={index} handleClick={handleDotClick}/>
+            <DotsBlock dotsCount={SLIDE_COUNT} currentIndex={index} handleClick={handleDotClick}/>
         </section>
     );
 }
