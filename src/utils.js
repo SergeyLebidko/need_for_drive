37126cdebebadd681f4901350b27767511f240c6
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-21 15:47:00 +0300

    v-01_f-01 Код формирования данных для слайдов вынесен в отдельную функцию

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+import slide0 from './content/images/slides/slide-0.png';
+import slide1 from './content/images/slides/slide-1.png';
+import slide2 from './content/images/slides/slide-2.png';
+import slide3 from './content/images/slides/slide-3.png';
+import {LANG_PACK} from "./langPack";
+
+export function createSliderData(lang) {
+    const SLIDE_COUNT = 4
+    const slidersImages = [slide0, slide1, slide2, slide3];
+    const slidersButtonColors = ['dark_green', 'teal', 'brown', 'purple'];
+    const {slidersText, slideButtonText} = LANG_PACK['Slider'][lang];
+    const slidersData = [];
+
+    for (let index = 0; index < SLIDE_COUNT; index++) slidersData.push({
+        ...slidersText[index],
+        image: slidersImages[index],
+        buttonColor: slidersButtonColors[index]
+    });
+
+    return {data: slidersData, buttonText: slideButtonText}
+}
