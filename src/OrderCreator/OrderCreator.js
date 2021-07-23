af1b8c3ef8dc97a3ab17edc92fa1df1b94c6055a
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-23 13:03:55 +0300

    v-02 Создан каркас для компонентов создания и просмотра заказа

---
+++ src/OrderCreator/OrderCreator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './OrderCreator.scss';
+
+function OrderCreator(){
+    return (
+        <div>
+            Здесь будет форма создания заказа
+        </div>
+    )
+}
+
+export default OrderCreator;
---
+++ src/OrderViewer/OrderViewer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './OrderViewer.scss';
+
+function OrderViewer() {
+    return (
+        <div>
+            Здесь будет окно просмотра сведений о заказе
+        </div>
+    )
+}
+
+export default OrderViewer;
