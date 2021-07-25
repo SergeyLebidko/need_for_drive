a4b0fe9c89b90993ff3a46e4d875aa6ace0e95b7
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-25 16:04:57 +0300

    v-02 Первая версия законченной верстки

--- src/common_components/OrderDetails/OrderDetails.js
+++ src/common_components/OrderDetails/OrderDetails.js
@@ -4,7 +4,18 @@ import './OrderDetails.scss';
 function OrderDetails() {
     return (
         <div className="order_details">
-            Здесь блок с деталями заказа
+            <header className="order_details__header">Ваш заказ</header>
+            <ul className="order_details__parameter_list">
+                <li className="order_details__parameter">
+                    <span className="order_details__parameter_name">
+                        Пункт выдачи
+                    </span>
+                    <span className="order_details__spacer"/>
+                    <span className="order_details__parameter_value">
+                        Ульяновск, Нариманова 42
+                    </span>
+                </li>
+            </ul>
         </div>
     );
 }
