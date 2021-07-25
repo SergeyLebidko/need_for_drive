3216bc8b2d8b126e1f5b09d5ca467bf14dffb622
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-25 17:49:28 +0300

    v-02 Первая версия верстки в LocationTab.js

--- src/order_ceator_components/tab_components/LocationTab/LocationTab.js
+++ src/order_ceator_components/tab_components/LocationTab/LocationTab.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import tempMap from '../../../content/images/map.png';
 import './LocationTab.scss';
 
 function LocationTab() {
     return (
         <div className="location_tab">
-            Это вкладка с данными о местоположении автомобиля
+            <table className="location_tab__input_block">
+                <tbody>
+                <tr>
+                    <td>
+                        <label htmlFor="location_tab_city_field">Город</label>
+                    </td>
+                    <td>
+                        <input
+                            id="location_tab_city_field"
+                            className="text_input_field"
+                            placeholder="Начните вводить город"
+                        />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label htmlFor="location_tab_point_field">Пункт выдачи</label>
+                    </td>
+                    <td>
+                        <input
+                            id="location_tab_point_field"
+                            className="text_input_field"
+                            placeholder="Начните вводить пункт выдачи"
+                        />
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <label className="location_tab__map_label">Выбрать на карте</label>
+            <img src={tempMap} className="location_tab__map"/>
         </div>
     )
 }
