d9526d67caabbf99470c356eef7dc886e2619614
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-25 19:23:54 +0300

    v-02 Удалена некорректная разметка и стили

--- src/order_ceator_components/tab_components/LocationTab/LocationTab.js
+++ src/order_ceator_components/tab_components/LocationTab/LocationTab.js
@@ -5,34 +5,6 @@ import './LocationTab.scss';
 function LocationTab() {
     return (
         <div className="location_tab">
-            <table className="location_tab__input_block">
-                <tbody>
-                <tr>
-                    <td>
-                        <label htmlFor="location_tab_city_field">Город</label>
-                    </td>
-                    <td>
-                        <input
-                            id="location_tab_city_field"
-                            className="text_input_field"
-                            placeholder="Начните вводить город"
-                        />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label htmlFor="location_tab_point_field">Пункт выдачи</label>
-                    </td>
-                    <td>
-                        <input
-                            id="location_tab_point_field"
-                            className="text_input_field"
-                            placeholder="Начните вводить пункт выдачи"
-                        />
-                    </td>
-                </tr>
-                </tbody>
-            </table>
             <label className="location_tab__map_label">Выбрать на карте</label>
             <img src={tempMap} className="location_tab__map"/>
         </div>
