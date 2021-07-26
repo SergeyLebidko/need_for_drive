d5483dd402fffda2dee2b249fa2d82897e1a4137
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-26 10:29:09 +0300

    v-02 Создана разметка и стили для формы ввода города и местоположения

--- src/order_ceator_components/tab_components/LocationTab/LocationTab.js
+++ src/order_ceator_components/tab_components/LocationTab/LocationTab.js
@@ -5,6 +5,24 @@ import './LocationTab.scss';
 function LocationTab() {
     return (
         <div className="location_tab">
+            <div className="location_tab__place_form">
+                <div className="location_tab__form_row">
+                    <div className="location_tab__form_label">
+                        Город
+                    </div>
+                    <div className="location_tab__form_field">
+                        <input className="text_input" placeholder="Начните вводить город ..."/>
+                    </div>
+                </div>
+                <div className="location_tab__form_row">
+                    <div className="location_tab__form_label">
+                        Пункт выдачи
+                    </div>
+                    <div className="location_tab__form_field">
+                        <input className="text_input" placeholder="Начните вводить пунк выдачи ..."/>
+                    </div>
+                </div>
+            </div>
             <label className="location_tab__map_label">Выбрать на карте</label>
             <img src={tempMap} className="location_tab__map"/>
         </div>
