0d334844ea853d081e4430d04fa560ad2200ddd4
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-26 14:19:50 +0300

    v-02 РСоздание данных для заголовков вкладок перенесено в отдельную функцию

--- src/order_ceator_components/OrderCreator/OrderCreator.js
+++ src/order_ceator_components/OrderCreator/OrderCreator.js
@@ -4,30 +4,13 @@ import Menu from '../../common_components/menu_components/Menu/Menu';
 import PageHeader from '../../common_components/PageHeader/PageHeader';
 import TabControl from '../TabControl/TabControl';
 import {createStoreConnectedComponent} from '../../store/connector';
-import {EXTRA_MODE, LOCATION_MODE, MODEL_MODE, TOTAL_MODE} from '../../settings';
+import {createTabTitlesData} from '../../utils';
 import './OrderCreator.scss';
 
 function OrderCreator({setTabItemsData}) {
 
     // При монтировании передаем нижележащим компонентам список вкладок
-    useEffect(() => setTabItemsData([
-        {
-            title: 'Местоположение',
-            boundMode: LOCATION_MODE
-        },
-        {
-            title: 'Модель',
-            boundMode: MODEL_MODE
-        },
-        {
-            title: 'Дополнительно',
-            boundMode: EXTRA_MODE
-        },
-        {
-            title: 'Итого',
-            boundMode: TOTAL_MODE
-        }
-    ]), []);
+    useEffect(() => setTabItemsData(createTabTitlesData()), []);
 
     return (
         <div className="order_creator">
