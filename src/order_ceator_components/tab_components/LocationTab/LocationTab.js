6dd6c47bb3b998abcd8b1e909af48757ef6564ce
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-26 14:43:29 +0300

    v-02 Форма выбора местоположения перенесена в отдельный компонент

--- src/order_ceator_components/tab_components/LocationTab/LocationTab.js
+++ src/order_ceator_components/tab_components/LocationTab/LocationTab.js
@@ -1,30 +1,12 @@
 import React from 'react';
+import PlaceForm from '../../../common_components/PlaceForm/PlaceForm';
 import tempMap from '../../../content/images/map.png';
 import './LocationTab.scss';
 
 function LocationTab() {
     return (
         <div className="location_tab">
-            <div className="location_tab__place_form">
-                <div className="location_tab__form_row">
-                    <div className="location_tab__form_label">
-                        Город
-                    </div>
-                    <div className="location_tab__form_field">
-                        <input className="text_input" placeholder="Начните вводить город ..."/>
-                        <span className="location_tab__clear_field_button">&#215;</span>
-                    </div>
-                </div>
-                <div className="location_tab__form_row">
-                    <div className="location_tab__form_label">
-                        Пункт выдачи
-                    </div>
-                    <div className="location_tab__form_field">
-                        <input className="text_input" placeholder="Начните вводить пункт выдачи ..."/>
-                        <span className="location_tab__clear_field_button">&#215;</span>
-                    </div>
-                </div>
-            </div>
+            <PlaceForm/>
             <label className="location_tab__map_label">Выбрать на карте</label>
             <img src={tempMap} className="location_tab__map"/>
         </div>
