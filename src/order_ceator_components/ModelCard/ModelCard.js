956901c53950945c6c2956466c2ebb590d6fdcd6
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-27 19:14:38 +0300

    v-02 Создан компонент ModelCard.js

---
+++ src/order_ceator_components/ModelCard/ModelCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import classNames from 'classnames';
+import {getFormattedPrice} from '../../utils';
+import './ModelCard.scss';
+
+function ModelCard({model, hasSelected, handleClick}) {
+    let cardClassNames = classNames({
+        'model_card': true,
+        'selected_card': hasSelected
+    });
+
+    return (
+        <div className={cardClassNames} onClick={() => handleClick(model)}>
+            <span className="model_card__title">
+                {model.title}
+            </span>
+            <span className="model_card__price">
+                {getFormattedPrice(model.costStart)} - {getFormattedPrice(model.costEnd)} &#8381;
+            </span>
+            <img src={model.file} className="model_card__photo" alt={model.title}/>
+        </div>
+    );
+}
+
+ModelCard.propTypes = {
+    model: propTypes.shape({
+        title: propTypes.string,
+        costStart: propTypes.number,
+        costEnd: propTypes.number,
+        file: propTypes.string
+    }),
+    hasSelected: propTypes.bool,
+    handleClick: propTypes.func
+}
+
+export default ModelCard;
--- src/order_ceator_components/ModelSelector/ModelSelector.js
+++ src/order_ceator_components/ModelSelector/ModelSelector.js
@@ -1,33 +1,24 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import {getFormattedPrice} from '../../utils';
+import ModelCard from '../ModelCard/ModelCard';
 import {createStoreConnectedComponent} from '../../store/connector';
 import './ModelSelector.scss';
 
 function ModelSelector({modelList}) {
-    let [selectedModelIndex, setSelectedModelIndex] = useState(null);
+    let [selectedModel, setSelectedModel] = useState(null);
 
-    let handleCardClick = index => setSelectedModelIndex(index);
-
-    let getCardClassNames = index => classNames({
-        'model_selector__card': true,
-        'selected_card': index === selectedModelIndex
-    });
+    let handleCardClick = model => setSelectedModel(model);
 
     return (
         <div className="model_selector">
             {modelList.map(
-                (model, index) =>
-                    <div key={model.title} className={getCardClassNames(index)} onClick={() => handleCardClick(index)}>
-                        <span className="model_selector__title">
-                            {model.title}
-                        </span>
-                        <span className="model_selector__price">
-                            {getFormattedPrice(model.costStart)} - {getFormattedPrice(model.costEnd)} &#8381;
-                        </span>
-                        <img src={model.file} className="model_selector__photo"/>
-                    </div>
+                model =>
+                    <ModelCard
+                        key={model.title}
+                        model={model}
+                        hasSelected={selectedModel === model}
+                        handleClick={handleCardClick}
+                    />
             )}
         </div>
     );
