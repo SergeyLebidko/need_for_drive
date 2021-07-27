79805669003937d19a1c315ec01b246c2039664b
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-27 12:51:28 +0300

    v-02 В PlaceSelector.js добавлены теги label

--- src/order_ceator_components/PlaceSelector/PlaceSelector.js
+++ src/order_ceator_components/PlaceSelector/PlaceSelector.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import './PlaceForm.scss';
+import './PlaceSelector.scss';
 
 function PlaceSelector() {
     return (
         <div className="place_form">
             <div className="place_form__form_row">
                 <div className="place_form__form_label">
-                    Город
+                    <label htmlFor="place_form_city_field">Город</label>
                 </div>
                 <div className="place_form__form_field">
-                    <input className="text_input" placeholder="Начните вводить город ..."/>
+                    <input
+                        id="place_form_city_field"
+                        className="text_input"
+                        placeholder="Начните вводить город ..."
+                    />
                     <span className="place_form__clear_field_button">&#215;</span>
                 </div>
             </div>
             <div className="place_form__form_row">
                 <div className="place_form__form_label">
-                    Пункт выдачи
+                    <label htmlFor="place_form_point_field">Пункт выдачи</label>
                 </div>
                 <div className="place_form__form_field">
-                    <input className="text_input" placeholder="Начните вводить пункт выдачи ..."/>
+                    <input
+                        id="place_form_point_field"
+                        className="text_input"
+                        placeholder="Начните вводить пункт выдачи ..."
+                    />
                     <span className="place_form__clear_field_button">&#215;</span>
                 </div>
             </div>
