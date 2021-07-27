0cdc0a3fdb3a555f00b241fc0ee9637919f120db
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-27 17:11:25 +0300

    v-02 Блоку с детализацией заказа добавлено прилипание

--- src/common_components/OrderDetails/OrderDetails.js
+++ src/common_components/OrderDetails/OrderDetails.js
@@ -12,31 +12,33 @@ function OrderDetails() {
 
     return (
         <div className="order_details">
-            <header className="order_details__header">Ваш заказ</header>
-            <ul className="order_details__parameter_list">
-                <li className="order_details__parameter">
+            <div className="order_details__content_block">
+                <header className="order_details__header">Ваш заказ</header>
+                <ul className="order_details__parameter_list">
+                    <li className="order_details__parameter">
                     <span className="order_details__parameter_name">
                         Пункт выдачи
                     </span>
-                    <span className="order_details__spacer"/>
-                    <span className="order_details__parameter_value">
+                        <span className="order_details__spacer"/>
+                        <span className="order_details__parameter_value">
                         Ульяновск, Нариманова 42
                     </span>
-                </li>
-                <li className="order_details__parameter">
+                    </li>
+                    <li className="order_details__parameter">
                     <span className="order_details__parameter_name">
                         Модель
                     </span>
-                    <span className="order_details__spacer"/>
-                    <span className="order_details__parameter_value">
+                        <span className="order_details__spacer"/>
+                        <span className="order_details__parameter_value">
                         Huyndai, i30 N
                     </span>
-                </li>
-            </ul>
-            <span className="order_details__price">Цена: от 8 000 до 12 000 &#8381;</span>
-            <button className={buttonClasses} disabled>
-                Выбрать модель
-            </button>
+                    </li>
+                </ul>
+                <span className="order_details__price">Цена: от 8 000 до 12 000 &#8381;</span>
+                <button className={buttonClasses} disabled>
+                    Выбрать модель
+                </button>
+            </div>
         </div>
     );
 }
