376b9d1a972488f335357ce47e251c7eb3a5cde4
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-29 12:22:31 +0300

    v-02 Правка htmlFor-параметров в SelectorItem.js

--- src/order_ceator_components/place_selector_components/SelectorItem/SelectorItem.js
+++ src/order_ceator_components/place_selector_components/SelectorItem/SelectorItem.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import randomstring from 'randomstring';
 import './SelectorItem.scss';
 
-function SelectorItem({caption, placeholder}){
+function SelectorItem({caption, placeholder}) {
+    let inputId = randomstring.generate('alphabetic');
+
     return (
         <div className="selector_item">
-                <div className="selector_item__label">
-                    <label htmlFor="">{caption}</label>
-                </div>
-                <div className="selector_item__field">
-                    <input
-                        id=""
-                        className="text_input"
-                        placeholder={placeholder}
-                    />
-                    <span className="selector_item__clear_button">&#215;</span>
-                </div>
+            <div className="selector_item__label">
+                <label htmlFor={inputId}>{caption}</label>
             </div>
+            <div className="selector_item__field">
+                <input
+                    id={inputId}
+                    className="text_input"
+                    placeholder={placeholder}
+                />
+                <span className="selector_item__clear_button">&#215;</span>
+            </div>
+        </div>
     )
 }
 
