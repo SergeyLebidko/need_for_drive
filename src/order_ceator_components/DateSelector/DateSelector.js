ce821b458ad480f791818c13f06fed874b33e93a
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-30 14:28:24 +0300

    v-02 Завершен второй этап настройки DateSelector.js

--- src/order_ceator_components/DateSelector/DateSelector.js
+++ src/order_ceator_components/DateSelector/DateSelector.js
@@ -12,43 +12,48 @@ function DateSelector() {
 
     return (
         <div className="date_selector">
-            <div>
-                <div>
-                    <label>С</label>
+            <h1 className="date_selector__caption">Дата аренды</h1>
+            <div className="date_selector__selector_container">
+                <div className="date_selector__row">
+                    <div className="date_selector__cell">
+                        <label>С</label>
+                    </div>
+                    <div className="date_selector__cell">
+                        <DatePicker
+                            selected={startDate}
+                            onChange={date => setStartDate(date)}
+                            showTimeSelect
+                            selectsStart
+                            startDate={startDate}
+                            endDate={endDate}
+                            dateFormat="Pp"
+                            timeIntervals={10}
+                            locale="ru-RU"
+                            timeCaption="Время"
+                            isClearable
+                        />
+                    </div>
                 </div>
-                <div>
-                    <DatePicker
-                        selected={startDate}
-                        onChange={date => setStartDate(date)}
-                        showTimeSelect
-                        selectsStart
-                        startDate={startDate}
-                        endDate={endDate}
-                        dateFormat="Pp"
-                        timeIntervals={10}
-                        locale="ru-RU"
-                        timeCaption="Время"
-                        isClearable
-                    />
-                </div>
-                <div>
-                    <label>По</label>
-                </div>
-                <div>
-                    <DatePicker
-                        selected={endDate}
-                        onChange={date => setEndDate(date)}
-                        showTimeSelect
-                        selectsEnd
-                        startDate={startDate}
-                        endDate={endDate}
-                        minDate={startDate}
-                        dateFormat="Pp"
-                        timeIntervals={10}
-                        locale="ru-RU"
-                        timeCaption="Время"
-                        isClearable
-                    />
+                <div className="date_selector__row">
+                    <div className="date_selector__cell">
+                        <label>По</label>
+                    </div>
+                    <div className="date_selector__cell">
+                        <DatePicker
+                            selected={endDate}
+                            onChange={date => setEndDate(date)}
+                            showTimeSelect
+                            selectsEnd
+                            startDate={startDate}
+                            endDate={endDate}
+                            minDate={startDate}
+                            dateFormat="Pp"
+                            timeIntervals={10}
+                            locale="ru-RU"
+                            timeCaption="Время"
+                            isClearable
+                        />
+                    </div>
                 </div>
             </div>
         </div>
