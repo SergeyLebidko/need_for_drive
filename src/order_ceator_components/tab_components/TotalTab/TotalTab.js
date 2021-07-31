853f721a6a9c16d1f5d17aa2c2df94994f57d34e
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-31 12:02:04 +0300

    v-02 Первая версия верстки для TotalTab.js

--- src/order_ceator_components/tab_components/TotalTab/TotalTab.js
+++ src/order_ceator_components/tab_components/TotalTab/TotalTab.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import car from '../../../content/images/car1.png';
 import './TotalTab.scss';
 
-function TotalTab(){
+function TotalTab() {
     return (
         <div className="total_tab">
-
+            <ul className="total_tab__details_block">
+                <li>
+                    <span>Hyundai, i30 N</span>
+                </li>
+                <li>
+                    <span>K 761 HA 73</span>
+                </li>
+                <li>
+                    <span>Топливо:</span><span>100%</span>
+                </li>
+                <li>
+                    <span>Доступна с:</span><span>01.08.2021</span>
+                </li>
+            </ul>
+            <div className="total_tab__photo_block">
+                <img src={car}/>
+            </div>
         </div>
     )
 }
