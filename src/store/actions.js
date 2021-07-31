9ef8583dec7ef6ce6eec9327bd2f0ea907cbeff0
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-31 15:10:53 +0300

    v-02 Правка значения константы определения города в actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,5 @@
 export const SET_LANG = 'set_lang';
-export const SET_CITY = 'define_city';
+export const SET_CITY = 'set_city';
 export const SET_SLIDER_DATA = 'set_slide_data';
 export const SET_TAB_ITEMS_DATA = 'set_tab_items_data';
 export const SET_CATEGORY_LIST = 'set_category_list';
