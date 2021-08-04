432bc7d7178a1b599f71f5ed3323d12aee17e472
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-04 11:44:58 +0300

    v-02 В OrderCreator добавлено меню

--- src/order_viewer_components/OrderViewer/OrderViewer.js
+++ src/order_viewer_components/OrderViewer/OrderViewer.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Menu from '../../common_components/menu_components/Menu/Menu';
+import PageHeader from '../../common_components/PageHeader/PageHeader';
 import './OrderViewer.scss';
 
 function OrderViewer({match}) {
     return (
-        <div>
-            Здесь будет окно просмотра сведений о заказе {match.params.orderId}
+        <div className="order_viewer">
+            <Menu/>
+            <section className="order_viewer__content">
+                <PageHeader/>
+                Здесь будет окно просмотра сведений о заказе {match.params.orderId}
+            </section>
         </div>
     )
 }
