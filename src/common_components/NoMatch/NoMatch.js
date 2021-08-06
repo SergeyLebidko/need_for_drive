f3eb5720e1dd9454631fa2a2656b7ec71c37fffa
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-06 11:42:43 +0300

    f-03 Добавлен компонент NoMatch.js

---
+++ src/common_components/NoMatch/NoMatch.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './NoMatch.scss';
+
+function NoMatch({history, location}) {
+    let handleToMainPageButtonClick = () => history.push('/');
+
+    return (
+        <div className="no_match">
+            <div className="no_match__content">
+                <h1 className="no_match__404">404</h1>
+                <h3 className="no_match__path">Страница {location.pathname} не найдена</h3>
+                <button
+                    className="button button_main_accent button_main_round_border"
+                    onClick={handleToMainPageButtonClick}
+                >
+                    На главную
+                </button>
+            </div>
+        </div>
+    )
+}
+
+NoMatch.propTypes = {
+    history: PropTypes.object,
+    location: PropTypes.object
+}
+
+export default NoMatch;
