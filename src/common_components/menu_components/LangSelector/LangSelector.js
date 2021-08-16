8285094b018a307d5a61a73c24ce4ef1c3cc7a95
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-16 18:38:09 +0300

    f-06_f-07 Корректировка const и let в LangSelector.js

--- src/common_components/menu_components/LangSelector/LangSelector.js
+++ src/common_components/menu_components/LangSelector/LangSelector.js
@@ -5,11 +5,11 @@ import {ENG, RUS, LANG_SWITCHER_MAP, LS_LANG_KEY} from '../../../settings';
 import './LangSelector.scss';
 
 function LangSelector({lang, setLang}) {
-    let getNextLangTitle = lang => ({[RUS]: 'Рус', [ENG]: 'Eng'}[LANG_SWITCHER_MAP[lang]]);
+    const getNextLangTitle = lang => ({[RUS]: 'Рус', [ENG]: 'Eng'}[LANG_SWITCHER_MAP[lang]]);
 
     // При смене языка - сохраняем выбор пользователя также и в local storage, чтобы он был доступен при следующем входе на сайт
-    let handleClick = () => {
-        let nextLang = LANG_SWITCHER_MAP[lang];
+    const handleClick = () => {
+        const nextLang = LANG_SWITCHER_MAP[lang];
         setLang(LANG_SWITCHER_MAP[lang]);
         localStorage.setItem(LS_LANG_KEY, nextLang);
     };
