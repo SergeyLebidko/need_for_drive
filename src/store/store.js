0e4a7eee4e67a03aac0dac4ded285257392ce5e3
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-16 19:21:38 +0300

    f-06_f-07 Рефакторинг использования let и const в store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,6 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import * as reducers from './reducers';
 
-let combinedReducer = combineReducers(reducers);
+const combinedReducer = combineReducers(reducers);
 const store = createStore(combinedReducer, {}, applyMiddleware(thunk));
 export default store;
